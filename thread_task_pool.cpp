6efa7fb33d0b5b04e8a9850fe7b11af0aef7db59
PeiWenZeng <zengpw@foxmail.com>
2013-12-06 17:50:03 +0800

    添加任务队列，完成度50%

--- thread_task_pool.cpp
+++ thread_task_pool.cpp
@@ -1,1 +1,25 @@
 #include "thread_task_pool.h"
+
+template<class T>
+int threadTaskPool<T>::init()
+{
+}
+
+template<class T>
+int threadTaskPool<T>::destroy()
+{
+
+}
+
+template<class T>
+int threadTaskPool<T>::push(T* task)
+{
+
+}
+
+template<class T>
+T* threadTaskPool<T>::pop()
+{
+
+}
+
--- thread_task_pool.h
+++ thread_task_pool.h
@@ -1,5 +1,34 @@
 #ifndef _THREAD_TASK_POOL_H_
 #define _THREAD_TASK_POOL_H_
 
+#include <list>
+
+template<class T>
+class threadTaskPool
+{
+public:
+	threadTaskPool()
+	{
+		init();
+	}
+
+	~threadTaskPool()
+	{
+		destroy();
+	}
+
+private:
+	/*the pool*/
+	std::list<T*> m_pool;
+public:
+	/*not thread safe,so you must use init()/destroy() in single(master) thread*/
+	int init();
+	int destroy();
+
+	/*manipulating pool*/
+	int push(T* task);
+	T* pop();
+};
+
 /*_THREAD_TASK_POOL_H_*/
 #endif
