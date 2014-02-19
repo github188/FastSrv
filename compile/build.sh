625144f4aacafd78c924a54d16004ba9fe0b15ef
PeiWenZeng <zengpw@foxmail.com>
2014-02-19 17:44:45 +0800

    update at Feb.19

---
+++ compile/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo "\n\t\t\t\t=============================cmake使用============================="
+echo "\n\t\t\t\t=========Ⅰ.在本目录下执行 \"cmake ./\"，生成 Makefile 文件========"
+echo "\n\t\t\t\t=========Ⅱ.执行\"gmake\"，编译===================================="
+echo "\n\t\t\t\t=========Ⅲ.清理直接执行本脚本即可================================="
+echo "\n"
+gmake clean
+rm -rf CMakeCache.txt Makefile cmake_install.cmake CMakeFiles
---
+++ example/ps_client.cpp
@@ -0,0 +1,11 @@
+/* 测试client端的例子,makefile统一在cmake目录下 */
+#include "../ps_api.hpp"
+
+int main()
+{
+	int fd = 0;
+
+	fd = socket_open("127.0.0.1",42890);
+	printf("\n==============fd: %d",fd);
+	printf("\n");
+}
