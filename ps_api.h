f58343fe7ff0a39b25e40026f897ca90d8577a08
PeiWenZeng <zengpw@foxmail.com>
2014-02-20 20:10:12 +0800

    完善socket

---
+++ ps_api.h
@@ -0,0 +1,7 @@
+
+#include "ps_define.h"
+#include "ps_socket_tcp_ipv4.hpp"
+#include "ps_socket_tcp_transport.hpp"
+#include "ps_socket_select.hpp"
+#include "ps_socket_epoll.hpp"
+
---
+++ ps_define.h
@@ -0,0 +1,19 @@
+#ifndef __PS_DEFINE_H__
+#define __PS_DEFINE_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <sys/types.h>
+/* socket */
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <sys/socket.h>
+
+// __PS_DEFINE_H__
+#endif
